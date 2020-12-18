58073a530774486cd1895d3d323421c5d203c48f
Jeshua Lin <jeslinmx@users.noreply.github.com>
2020-12-18 19:58:20 +0800

    day 17 removed numpy and curse of dimensionality

--- 17.py
+++ 17.py
@@ -2,32 +2,38 @@
 
 # Input
 from sys import stdin
-import numpy as np
-initial = np.array([
-    [char == "#" for char in line.strip()]
-    for line in stdin
-])
+initial = [
+    (y, x)
+    for y, line in enumerate(stdin)
+    for x, char in enumerate(line.strip())
+    if char == "#"
+]
 
 # Part 1
+from operator import add
 from itertools import product
-def evolve(state, timesteps):
+from collections import defaultdict
+def evolve(points, timesteps):
     if timesteps <= 0:
-        return state
+        return points
+    nearby_active = defaultdict(int)
+    for point in points:
+        for offset in product([-1, 0, 1], repeat=len(point)):
+            nearby_active[tuple(map(add, point, offset))] += 1
     # the evolution rules can be restated, in terms of nearby cubes (including
     # the target cube), as follows:
     # 1. cubes which have 3 nearby active cubes will be active, and
     # 2. currently active cubes which have 4 nearby active cubes will be active
-    nearby_active = sum((
-        np.pad(state, pad_width, mode="constant", constant_values=0)
-        for pad_width in product([(0, 2), (1, 1), (2, 0)], repeat=state.ndim)
-    ))
-    next_state = (
-        (nearby_active == 3) # rule 1
-        | (np.pad(state, pad_width=1) & (nearby_active == 4)) # rule 2
-    )
+    next_state = [
+        point
+        for point, nearby in nearby_active.items()
+        if nearby == 3 or (point in points and nearby == 4)
+    ]
     return evolve(next_state, timesteps - 1)
 
-print(evolve(initial[np.newaxis,:], 6).sum())
+initial_cube = [(0, y, x) for y, x in initial]
+print(len(evolve(initial_cube, 6)))
 
 # Part 2
-print(evolve(initial[np.newaxis, np.newaxis,:], 6).sum())
+initial_hypercube = [(0, 0, y, x) for y, x in initial]
+print(len(evolve(initial_hypercube, 6)))
